bfc09fc7db48bce42ef17ab86b3d4a2a23742d34
Philippe Gibert <philippe.gibert@gmail.com>
2023-01-16 23:48:21 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# MinioCurl
+
+Project that tests upload and get file infos of S3 endpoint, with defined and verified metadata.
+
+## Command
+
+```bash
+./s3-curl.sh schema s3Url s3Key s3Secret bucketName FileName
+```
+
+## Test
+exemple : 
+```bash
+docker-compose up -d
+./s3-curl.sh http localhost:9000 test testAzertyuiop bucket-test test.txt
+```
---
+++ s3-curl.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# Usage: ./s3-curl.sh http localhost:9000 test testAzertyuiop bucket-test test.txt
+
+schema=$1
+host=$2
+s3_key=$3
+s3_secret=$4
+
+bucket=$5
+file=$6
+
+resource="/${bucket}/${file}"
+content_type="application/octet-stream"
+date=`date -R`
+metadataKey="x-amz-meta-custom"
+metadataValue="custom-value"
+metadata="${metadataKey}:${metadataValue}"
+
+_signature="PUT\n\n${content_type}\n${date}\n${metadata}\n${resource}"
+signature=`echo -en ${_signature} | openssl sha1 -hmac ${s3_secret} -binary | base64`
+curl -X PUT -T "${file}" \
+          -H "Host: ${host}" \
+          -H "Date: ${date}" \
+          -H "Content-Type: ${content_type}" \
+          -H "Authorization: AWS ${s3_key}:${signature}" \
+          -H "${metadata}" \
+          ${schema}://${host}${resource}
+
+
+content_type="text/plain"
+date=`date -R`
+_signature="GET\n\n${content_type}\n${date}\n${resource}"
+signature=`echo -en ${_signature} | openssl sha1 -hmac ${s3_secret} -binary | base64`
+response=$(curl -vs  -X GET -H "Host: ${host}" \
+          -H "Date: ${date}" \
+          -H "Content-Type: ${content_type}" \
+          -H "Authorization: AWS ${s3_key}:${signature}" \
+          ${schema}://${host}${resource}  2>&1 >/dev/null)
+
+# echo  "$response"
+if [[ "$response" == *"${metadataKey}"* ]]; then
+  echo "Metadata found"
+else
+  echo "Metadata not found"
+fi
